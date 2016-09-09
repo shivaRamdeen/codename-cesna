dd3d6fa6c4927e43725b13c0eba215bf5a4cb487
Shiva Ramdeen <shiva.ramdeen@outlook.com>
2016-09-09 12:19:22 -0400

    added unit test for authentication testpoint

---
+++ tests/unit-token-based-auth.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Invalid, without password/credentials
+curl http://localhost:80/v1.0/test
+
+# Valid with Password
+curl -u shiva.ramdeen@outlook.com:password http://localhost:80/v1.0/test
