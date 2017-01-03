7ebe36b2143c222a4b85bb304b41627ea1c75b00
Shiva Ramdeen <shiva.ramdeen@outlook.com>
2017-01-03 16:20:56 -0400

    tested install script and fixed errors. Updated install script prompt

--- install.sh
+++ install.sh
@@ -3,15 +3,21 @@
 #automatic installation of the server and its dependencies.
 #Application will be containerized in the future.
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
+#sudo apt-get update -y
+#sudo apt-get upgrade -y
 sudo apt-get install python-pip -y
 sudo pip install --upgrade pip
 sudo apt-get install mysql-server -y
-sudo apt-get intall libmysqlclient-dev -y
+sudo apt-get install libmysqlclient-dev -y
 
+echo ...
+echo ...
+echo ...
 echo "We are now going to secure mysql."
 echo "Please ensure that your root password matches the msql connection string in 'database.py' and all other files"
+echo ...
+echo ...
+echo ...
 
 #waiting for user to see our prompt
 sleep 3
@@ -20,6 +26,11 @@ sudo mysql_secure_installation
 sudo pip install -r requirements.txt
 
 #instructions for final setup
+echo ...
+echo ...
+echo !!!IMPORTANT INSTRUCTIONS!!!
 echo Run the following command to create the default database
 echo "mysql -u root -e 'CREATE DATABASE hustle' -p"
 echo "Then run "sudo python database.py" to create the database tables"
+echo ...
+sleep 3
